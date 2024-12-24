2e7700f8b0700721660acb14a42d3e64ba241fb7
TGerasimova <tessagerasimova@gmail.com>
2024-12-24 18:53:26 +0200

    Решение задачи

--- ЕГКР-21-12-24/task-5-19237.py
+++ ЕГКР-21-12-24/task-5-19237.py
@@ -1,3 +1,20 @@
-print('hi')
+def convert(num, sys):
+    r = ''
+    while num:
+        r += str(num % sys)
+        num //= sys
+    return r[::-1]
 
-print('world')
+ans = []
+for n in range(1, 10000):
+    r = convert(n, 3)
+    if n % 3 == 0:
+        r = r + r[-2:]
+    else:
+        summ = sum(map(int, r))
+        summ = convert(summ, 3)
+        r = r + summ
+    r = int(r, 3)
+    if r > 220 and r % 2 == 0:
+        ans.append(r)
+print(min(ans))
