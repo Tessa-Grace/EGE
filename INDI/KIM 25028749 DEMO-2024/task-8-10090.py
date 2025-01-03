5de262065b8057a4024fbddcd62df47c8451f27d
Tessa-Grace <tessagerasimova@gmail.com>
2025-01-03 21:56:36 +0200

    работа над ошибками

--- INDI/KIM 25028749 DEMO-2024/task-8-10090.py
+++ INDI/KIM 25028749 DEMO-2024/task-8-10090.py
@@ -1,14 +1,14 @@
-from itertools import product
+from itertools import permutations
 
 count = 0
-for val in product('01234567', repeat=8):
+for val in permutations('01234567', 5):
     val = ''.join(val)
-    if val[0] != '0' and val[0] != val[1] != val[2] != val[3] != val[4] != val[5]:
+    if val[0] != '0' and len(val) == len(set(val)) and val.count('1') == 0:
         val = val.replace('0', '*').replace('2', '*').replace('4', '*').replace('6', '*')
         val = val.replace('1', '!').replace('3', '!').replace('5', '!').replace('7', '!')
-        if '**!!' not in val and '!!**' not in val:
+        if '**' not in val and '!!' not in val:
             count += 1
 print(count)
 
-# otvet: 4264624
+# otvet: 504
 
