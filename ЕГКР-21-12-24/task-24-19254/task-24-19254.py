334e445ebbf2473db2dc6ac1719db701b8d9e132
Tessa-Grace <tessagerasimova@gmail.com>
2025-01-03 19:09:14 +0200

    решение задачи 19254 (ошибка, надо разобрать решение)

---
+++ ЕГКР-21-12-24/task-24-19254/task-24-19254.py
@@ -0,0 +1,32 @@
+with open('24_19254.txt') as file:
+    data = file.readline()
+
+data = data.replace('FSRQ', '*')
+data = data.split('*')
+
+ans = 0
+for i in range(len(data) - 80):
+    ans = max(ans, len('*'.join(data[i:i+81])))
+print(ans)
+
+#otvet: 2133 ОШИБКА!!!
+'''
+Предлагают такое решение (переписала немного под себя):
+
+with open('24_19254.txt') as file:
+    data = file.readline()
+count, length, maxx = 0, 0, 0
+
+for i in range(3, len(data)):
+    if data[i-3] + data[i-2] + data[i-1] + data[i] == 'FSRQ':
+        count += 1
+    while count > 80:
+        if data[i] + data[i+1] + data[i+2] + data[i+3] == 'FSRQ':
+            count -= 1
+        length += 1
+    if count == 80:
+        maxx = max(maxx, i-length+1)
+print(maxx)
+
+#otvet: 2379
+'''
