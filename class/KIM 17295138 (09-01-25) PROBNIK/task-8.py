5f33eb8411d361a750fc2e4e5a24f163d1d605ae
Tessa-Grace <tessagerasimova@gmail.com>
2025-01-09 20:25:00 +0200

    исправление ошибки #8 из зимнего пробника

--- class/KIM 17295138 (09-01-25) PROBNIK/task-8.py
+++ class/KIM 17295138 (09-01-25) PROBNIK/task-8.py
@@ -2,10 +2,10 @@ from itertools import product
 from string import ascii_uppercase
 
 count = 0
-for val in product(ascii_uppercase[:7], repeat=6):
+for val in product(ascii_uppercase[:6], repeat=6):
     val = ''.join(val)
-    if val[0] not in 'ae' and val[-1] not in 'ae':
+    if val[0] not in 'AE' and val[-1] not in 'AE':
         count += 1
 print(count)
 
-# otvet: 117649
+# otvet: 20736
