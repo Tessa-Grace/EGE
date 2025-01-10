806f6fa1e284029352866f6c97c281cca0d55be1
Tessa-Grace <tessagerasimova@gmail.com>
2025-01-10 13:52:40 +0200

    решение 19234 #2 программно

---
+++ INDI/#2/task2-19234.py
@@ -0,0 +1,14 @@
+from itertools import *
+
+def f(w, x, y, z):
+    return not((not x or y) and not w) or not(z and not(y and w))
+
+for a1, a2, a3, a4, a5, a6, a7 in product([0, 1], repeat=7):
+    table = [(0, a1, a2, 1), (a3, 1, a4, a5), (1, 0, a6, a7)]
+    if len(table) == len(set(table)):
+        for p in permutations('wxyz'):
+            if [f(**dict(zip(p, r))) for r in table] == [0, 0, 0]:
+                print(p)
+
+# ('y', 'x', 'w', 'z')
+# otvet: yxwz
