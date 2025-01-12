c6c0e63458a14fa029f29361f6c6d0ef18ebef7f
Tessa-Grace <tessagerasimova@gmail.com>
2025-01-12 17:53:37 +0200

    solvation of 17518

---
+++ INDI/#5/task-5-17518.py
@@ -0,0 +1,13 @@
+ans = []
+for n in range(1, 10000):
+    r = bin(n)[2:]
+    if sum(map(int, r)) % 2 == 0:
+        r = '10' + r[2:] + '0'
+    else:
+        r = '11' + r[2:] + '1'
+    r = int(r, 2)
+    if r > 50:
+        ans.append(n)
+print(min(ans))
+
+# otvet: 19
