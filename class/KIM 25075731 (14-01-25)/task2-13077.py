34dea402ca401529a04118ac22b6211da8dbd940
TGerasimova <tessagerasimova@gmail.com>
2025-01-14 20:02:38 +0200

    to finish 13077!

---
+++ class/KIM 25075731 (14-01-25)/task2-13077.py
@@ -0,0 +1,18 @@
+from itertools import *
+
+def f1(w, x, y, z):
+    return (w == x) and (y <= z)
+
+def f2(w, x, y, z):
+    return (w <= x) <= (y == x)
+
+for a1, a2, a3, a4, a in product([0, 1], repeat=5):
+    table= [
+        (1, a1, 1, 1),
+        (a2, 1, 0, 0),
+        (a3, 0, 0, a4)
+    ]
+    for p in permutations('wxyz'):
+        if [f1(**dict(zip(p, r))) for r in table] == [1, 1, 0] and \
+                [f2(**dict(zip(p, r))) for r in table] == [1, 1, 0] == [0, a, 0]:
+            print(*p)
