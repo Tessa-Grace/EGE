a26158c3fad16bc33bcd3108fb31a204bc9c13fa
TGerasimova <tessagerasimova@gmail.com>
2025-01-14 19:18:48 +0200

    solution of 9733

--- class/KIM 25075731 (14-01-25)/task-2-9733.py
+++ class/KIM 25075731 (14-01-25)/task-2-9733.py
@@ -5,8 +5,9 @@ def f(w, x, y, z):
 
 for a1, a2, a3, a4 in product([0, 1], repeat=4):
     table = [(a1, a2, 0, 1), (1, 0, a3, 1), (1, 1, 0, a4)]
-    for p in permutations('wxyz'):
-        if [f(**dict(zip(p, r))) for r in table] == [0, 0, 0]:
-            print(*p)
+    if len(table) == len(set(table)):
+        for p in permutations('wxyz'):
+            if [f(**dict(zip(p, r))) for r in table] == [0, 0, 0]:
+                print(*p)
 
 #otvet: y x w z
