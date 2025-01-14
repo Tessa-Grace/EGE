74c0fdbc8539ca7448f2e4134b029e63a685d010
TGerasimova <tessagerasimova@gmail.com>
2025-01-14 19:21:55 +0200

    solution of 4582

--- class/KIM 25075731 (14-01-25)/task-2-4582.py
+++ class/KIM 25075731 (14-01-25)/task-2-4582.py
@@ -12,3 +12,17 @@ for w in range(2):
 # 0 1 0 1
 # 1 1 0 1
 # otvet: wzyx
+
+'''
+from itertools import *
+
+def f(w, x, y, z):
+    return not(w <= z) or (x <= y) or not x
+    
+for a1, a2, a3, a4, a5, a6, a7 in product([0, 1], repeat=7):
+    table = [(1, a1, a2, a3), (0, 1, 0, a4), (a5, 0, a6, a7)]
+    if len(table) == len(set(table)):
+        for p in permutations('wxyz'):
+        if [f(**dict(zip(p, r))) for r in table]== [0, 0, 0]:
+            print(*p)
+'''
