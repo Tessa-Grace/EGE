96e028edfb0ee9aefa9f63f5f844ab72e3c520ba
Tessa-Grace <tessagerasimova@gmail.com>
2025-01-15 23:10:28 +0200

    solution of 18120

--- INDI/#8/task-8-18120.py
+++ INDI/#8/task-8-18120.py
@@ -3,7 +3,7 @@ from itertools import product
 alph = sorted('ПРЕСТОЛ')
 gl = 'ЕО'
 
-
+# ans = []
 count = 0
 for pos, val in enumerate(product(alph, repeat=5), start=1):
     val = ''.join(val)
@@ -11,6 +11,8 @@ for pos, val in enumerate(product(alph, repeat=5), start=1):
             (val.count('П') + val.count('Р') + \
             val.count('С') + val.count('Т') + \
             val.count('Л')) <= 3:
+        # или if sum([1 for i in val if i in sogl]) <= 3:
+            # ans.append(pos)
         count += 1
 print(count)
 
