bf27b0d65bbbe6dd5921859cf205e66cce324cf3
Tessa-Grace <tessagerasimova@gmail.com>
2025-01-15 23:00:17 +0200

    solution of 18120

---
+++ INDI/#8/task-8-18120.py
@@ -0,0 +1,17 @@
+from itertools import product
+
+alph = sorted('ПРЕСТОЛ')
+gl = 'ЕО'
+
+
+count = 0
+for pos, val in enumerate(product(alph, repeat=5), start=1):
+    val = ''.join(val)
+    if val[-1] in gl and pos % 2 != 1 and \
+            (val.count('П') + val.count('Р') + \
+            val.count('С') + val.count('Т') + \
+            val.count('Л')) <= 3:
+        count += 1
+print(count)
+
+# otvet: 1776
