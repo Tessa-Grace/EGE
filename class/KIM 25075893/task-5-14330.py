aecf80ffed61abd19c598fd1f3eb790e5f8bd794
Tessa-Grace <tessagerasimova@gmail.com>
2025-01-17 16:28:26 +0200

    solution of 14430

---
+++ class/KIM 25075893/task-5-14330.py
@@ -0,0 +1,18 @@
+ans = []
+for num in range(10_000, 100_000):
+    num = str(num)
+    kv = (int(min(num)) + int(max(num))) ** 2
+    mult = 1
+    for i in num:
+        if int(i) % 2 == 0:
+            mult *= int(i)
+    if kv < mult:
+        res = str(mult) + str(kv)
+    else:
+        res = str(kv) + str(mult)
+    if res == '12116':
+        ans.append(num)
+print(min(ans))
+
+# otvet: 22229
+
