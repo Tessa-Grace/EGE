51741eabd38f15182ff982816d38659d65d81d19
TGerasimova <tessagerasimova@gmail.com>
2025-01-17 18:39:38 +0200

    solution of 11292

--- class/KIM 25075893/task-8-11292.py
+++ class/KIM 25075893/task-8-11292.py
@@ -5,13 +5,15 @@ alph = '0123456789ABCDEF'
 count = 0
 for val in product(alph, repeat=5):
     val = ''.join(val)
-    if val[0] != 0 and val.count('6') == 2:
+    if val[0] != '0' and val.count('6') == 2:
         val = val.replace('0', '*').replace('2', '*')
         val = val.replace('4', '*').replace('8', '*')
         val = val.replace('A', '*').replace('C', '*')
         val = val.replace('E', '*')
+        #for i in '02468ACE':
+            #val = val.replace(i, '*')
         if '*6' not in val and '6*' not in val and '66' not in val:
             count += 1
 print(count)
 
-# otvet: 4416 ОШИБКА
+# otvet: 4352
