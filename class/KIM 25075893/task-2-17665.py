fb96fbeed199741693f3206a0b1ef90f0b567629
Tessa-Grace <tessagerasimova@gmail.com>
2025-01-17 15:33:41 +0200

    solution of 17665

--- class/KIM 25075893/task-2-17665.py
+++ class/KIM 25075893/task-2-17665.py
@@ -1,3 +1,4 @@
+# 1 method
 from itertools import *
 
 def f(w, x, y, z):
@@ -13,4 +14,21 @@ for a1, a2, a3, a4, a5, a6, a7 in product([0, 1], repeat=7):
         for p in permutations('wxyz'):
             if [f(**dict(zip(p, r))) for r in table] == [0, 0, 0]:
                 print(*p)
+# otvet: zyxw
+
+# 2 method
+print('w x y z')
+for w in 0, 1:
+    for x in 0, 1:
+        for y in 0, 1:
+            for z in 0, 1:
+                f = (z <= (not(y <= x))) or w
+                if not f:
+                    print(w, x, y, z)
+
+# w x y z
+# 0 0 0 1
+# 0 1 0 1
+# 0 1 1 1
+
 # otvet: zyxw
