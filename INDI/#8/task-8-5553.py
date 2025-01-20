2230a93664c39deb4e1968a113abf226883b37e7
Tessa-Grace <tessagerasimova@gmail.com>
2025-01-20 21:50:04 +0200

    solution of 5553 permutations

--- INDI/#8/task-8-5553.py
+++ INDI/#8/task-8-5553.py
@@ -8,3 +8,15 @@ for val in set(permutations('СОТОЧКА', 7)):
 print(count)
 
 # otvet: 1800
+'''
+from itertools import *
+
+combs = ['АА', 'ОО', 'ОА', 'АО']
+
+count = 0
+for val in set(permutations('СОТОЧКА')):
+    val = ''.join(val)
+    if any(i in val for i in combs):
+        count += 1
+print(count)
+'''
