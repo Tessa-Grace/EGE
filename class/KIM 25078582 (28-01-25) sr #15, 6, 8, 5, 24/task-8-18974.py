82535e59e12ae4033cf2a6c3460bfb9b9d0e21b1
TGerasimova <tessagerasimova@gmail.com>
2025-01-28 19:03:23 +0200

    TO SOLVE

---
+++ class/KIM 25078582 (28-01-25) sr #15, 6, 8, 5, 24/task-8-18974.py
@@ -0,0 +1,13 @@
+from itertools import *
+from string import digits, ascii_uppercase
+
+alph = digits + ascii_uppercase
+
+ans = []
+count = 0
+for i in product(alph[:25], repeat=4):
+    if val[0] != '0':
+        for i in val:
+            if i in alph[]
+
+# ДОРЕШАТЬ
