6b6fbb59600a33e6280095c7bf900e2c490450f9
Tessa-Grace <tessagerasimova@gmail.com>
2025-01-29 19:02:58 +0200

    solution of 9783

---
+++ INDI/#14/task-14-9783.py
@@ -0,0 +1,15 @@
+from string import digits, ascii_uppercase
+
+alph = digits + ascii_uppercase
+
+ans = []
+for x in alph[:22]:
+    num1 = int('18' + x + '89957', 22)
+    num2 = int('80' + x + '33', 22)
+    num3 = int('521' + x + '6', 22)
+    num = num1 + num2 + num3
+    if num % 21 == 0:
+        ans.append(num // 21)
+print(min(ans))
+
+# otvet: 162947670
