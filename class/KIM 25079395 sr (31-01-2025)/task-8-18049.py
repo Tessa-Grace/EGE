41198129631ea73e238080d2fc38d0f11c90e022
TGerasimova <tessagerasimova@gmail.com>
2025-01-31 19:13:24 +0200

    solution of 18049

--- class/KIM 25079395 sr (31-01-2025)/task-8-18049.py
+++ class/KIM 25079395 sr (31-01-2025)/task-8-18049.py
@@ -6,8 +6,8 @@ count = 0
 for val in product(alph, repeat=7):
     val = ''.join(val)
     if val[0] != '0' and val[0] != '2' and val[0] != '4' and val[0] != '6':
-        if val[-3] != val[-2] and val[-2] != val[-1] and val[-3] != val[-1]:
+        if val[-3] != val[-2] or val[-2] != val[-1] or val[-3] != val[-1]:
             count += 1
 print(count)
 
-# otvet: 1837080
+# otvet: 2624400
