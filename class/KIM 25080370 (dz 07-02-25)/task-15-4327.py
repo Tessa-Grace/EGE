c3d53da302ff098a5c3b9f0a2cf9571db01c2046
Tessa-Grace <tessagerasimova@gmail.com>
2025-02-09 18:19:20 +0200

    solution of 4327

--- class/KIM 25080370 (dz 07-02-25)/task-15-4327.py
+++ class/KIM 25080370 (dz 07-02-25)/task-15-4327.py
@@ -5,14 +5,14 @@ def f(x):
     q = 41 <= x <= 54
     r = 6 <= x <= 53
     a = a1 <= x <= a2
-    return ((not p) <= q) and r and not a
+    return ((not p) <= q) and r and (not a)
 
 line = [x / 10 for x in range(4 * 10, 55 * 10)]
 ans = []
 for a1, a2 in combinations(line, 2):
-    if not [f(x) for x in line]:
+    if all(f(x) == 0 for x in line):
         ans.append(a2 - a1)
 
 print(min(ans))
 
-# otvet: ОШИБКА !!
+# otvet: 47
