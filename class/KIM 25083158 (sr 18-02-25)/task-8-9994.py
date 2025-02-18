7196fddad486fcdbba3c51375081846b455192fa
Tessa-Grace <tessagerasimova@gmail.com>
2025-02-18 18:14:55 +0200

    solution of 9994

---
+++ class/KIM 25083158 (sr 18-02-25)/task-8-9994.py
@@ -0,0 +1,10 @@
+from itertools import product
+
+alph = sorted('ШКОЛА')
+
+for pos, val in enumerate(product(alph, repeat=5), start=1):
+    val = ''.join(val)
+    if val == 'ШАЛАШ':
+        print(pos)
+
+# otvet: 2555
