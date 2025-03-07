da3d7f3d709216e0ca09dfa2b7fcaeb133fb5f81
Tessa-Grace <tessagerasimova@gmail.com>
2025-03-07 11:23:24 +0200

    solution of 20196

---
+++ class/KIM 25086062 (dz 07-03-25)/task-16-20196.py
@@ -0,0 +1,12 @@
+from sys import setrecursionlimit
+setrecursionlimit(10000)
+
+def f(n):
+    if n < 110:
+        return n
+    if n >= 110:
+        return n + f(n -1)
+
+print(f(2025) - f(2021))
+
+# otvet: 8094
