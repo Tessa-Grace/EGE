683636022ed0a40c76ddc00df6dde450f3291545
Tessa-Grace <tessagerasimova@gmail.com>
2025-03-14 18:36:58 +0200

    solution of 14952

--- class/KIM 25088217 (sr 14-03-25)/task-17-14952/task-17-14952.py
+++ class/KIM 25088217 (sr 14-03-25)/task-17-14952/task-17-14952.py
@@ -7,15 +7,15 @@ ans = []
 for i in range(len(data) - 2):
     num1, num2, num3 = data[i: i + 3]
 
-    u1 = len(str(abs(num1))) == 4 and i % 2 == 0
-    u2 = len(str(abs(num2))) == 4 and i % 2 == 0
-    u3 = len(str(abs(num3))) == 4 and i % 2 == 0
+    u1 = len(str(abs(num1))) == 4 and num1 % 2 == 0
+    u2 = len(str(abs(num2))) == 4 and num2 % 2 == 0
+    u3 = len(str(abs(num3))) == 4 and num3 % 2 == 0
     summ = num1 + num2 + num3
-    f1 = u1 + u2 + u3 == 1
+    f1 = u1 + u2 + u3 <= 1
     f2 = summ <= maxx_121
 
     if f1 and f2:
         ans.append(summ)
 print(len(ans), max(ans))
 
-# otvet: 1110 18313
+# otvet: 5211 20116
