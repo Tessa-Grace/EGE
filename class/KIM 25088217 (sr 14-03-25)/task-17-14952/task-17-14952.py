d20859436f61a3402356e1a3feaf759e1ffc73d0
Tessa-Grace <tessagerasimova@gmail.com>
2025-03-14 18:30:08 +0200

    solution of 14952

---
+++ class/KIM 25088217 (sr 14-03-25)/task-17-14952/task-17-14952.py
@@ -0,0 +1,21 @@
+with open('17_14952.txt') as file:
+    data = [int(i) for i in file]
+
+maxx_121 = max(i for i in data if str(i)[-3:] == '121')
+
+ans = []
+for i in range(len(data) - 2):
+    num1, num2, num3 = data[i: i + 3]
+
+    u1 = len(str(abs(num1))) == 4 and i % 2 == 0
+    u2 = len(str(abs(num2))) == 4 and i % 2 == 0
+    u3 = len(str(abs(num3))) == 4 and i % 2 == 0
+    summ = num1 + num2 + num3
+    f1 = u1 + u2 + u3 == 1
+    f2 = summ <= maxx_121
+
+    if f1 and f2:
+        ans.append(summ)
+print(len(ans), max(ans))
+
+# otvet: 1110 18313
