ce8489d9872a079cde6b9ef48f08f1042a70b52f
Tessa-Grace <tessagerasimova@gmail.com>
2025-03-14 19:16:34 +0200

    solution of 6635

---
+++ class/KIM 25088219 (14-03-25) #23 hard/task-23-6635.py
@@ -0,0 +1,26 @@
+# ______________________________________________________________
+#                            1 METHOD
+# ______________________________________________________________
+def f(cur, count=0):
+    if count == 13 and cur < 0:
+        ans.append(cur)
+        return 1
+    if count == 13 and cur >= 0:
+        return 0
+    return f(cur - 3, count + 1) + f(cur * (-3), count + 1)
+
+ans = []
+print(f(333), len(set(f(333))))
+# ______________________________________________________________
+#                            2 METHOD
+# ______________________________________________________________
+def f(cur, count=0):
+    if count == 13 and cur < 0:
+        return {cur}
+    if count == 13 and cur >= 0:
+        return set()
+    return f(cur - 3, count + 1) | f(cur * (-3), count + 1)
+
+print(len(f(333)))
+
+# otvet: 4096 2224
