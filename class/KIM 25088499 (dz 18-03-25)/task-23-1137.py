b325571e431a3fbf139e146fbc9aabc2a9aff02d
TGerasimova <tessagerasimova@gmail.com>
2025-03-18 18:12:49 +0200

    solution of 1137

--- class/KIM 25088499 (dz 18-03-25)/task-23-1137.py
+++ class/KIM 25088499 (dz 18-03-25)/task-23-1137.py
@@ -4,12 +4,12 @@ setrecursionlimit(100000)
 def f(cur, fin):
     if cur == fin:
         return 1
-    if cur > fin:
+    if int(cur, 2) > int(fin, 2):
         return 0
-    return f(cur + 1, fin) + \
-        f(int(str(cur) + '0'), fin) + \
-        f(int(str(cur) + '1'), fin)
+    return f(bin(int(cur, 2)+1)[2:], fin) + \
+        f(cur + '0', fin) + \
+        f(cur + '1', fin)
 
-print(f(100, 11101))
+print(f('100', '11101'))
 
-# otvet: 4687 MISTAKE
+# otvet: 79
