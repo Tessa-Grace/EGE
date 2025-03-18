73e8402a9a8f6c41a1938bb133f56853d1adb188
TGerasimova <tessagerasimova@gmail.com>
2025-03-18 19:17:17 +0200

    solution of 17625

---
+++ class/KIM 25088974 (sr 18-03-25)/task-6-17625.py
@@ -0,0 +1,33 @@
+from turtle import *
+
+tracer(0)
+m = 10
+lt(90)
+
+for i in range(10):
+    fd(22*m)
+    rt(90)
+    fd(16*m)
+    rt(90)
+
+up()
+fd(1*m)
+rt(90)
+fd(1*m)
+lt(90)
+
+down()
+for i in range(10):
+    fd(72*m)
+    rt(90)
+    fd(79*m)
+    rt(90)
+
+up()
+for x in range(-20, 20):
+    for y in range(-20, 25):
+        goto(x*m, y*m)
+        dot(3, 'white')
+done()
+# 15 * 21 = 315
+# otvet: 315
