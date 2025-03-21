4929b324bbf2facb95c79900d35e1cef76fa4b44
Tessa-Grace <tessagerasimova@gmail.com>
2025-03-21 18:35:16 +0200

    solution of 18580

--- class/KIM 25089715 (sr 21-03-25)/task-14-18580.py
+++ class/KIM 25089715 (sr 21-03-25)/task-14-18580.py
@@ -1,7 +1,7 @@
 from string import printable
 
 ans = []
-for x in printable[:26]:
+for x in printable[:25]:
     num1 = int('A4' + x + '7F2', 25)
     num2 = int('N' + x + 'G5' + x + 'H', 25)
     num3 = int('74' + x + 'M26', 25)
@@ -9,3 +9,5 @@ for x in printable[:26]:
     if int(num) % 24 == 0:
         ans.append(num // 24)
 
+print(max(ans))
+# otvet: 16751575
