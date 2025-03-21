0f6678f1336e67421be3b7940c0f6675fac60517
Tessa-Grace <tessagerasimova@gmail.com>
2025-03-21 18:32:09 +0200

    solution of 18580

---
+++ class/KIM 25089715 (sr 21-03-25)/task-17-17636.py
@@ -0,0 +1,17 @@
+with open('17_17636.txt') as file:
+    data = [int(i) for i in file]
+
+max_3 = max([i for i in data if str(i)[-1] == '3' and len(str(abs(i))) == 3])
+ans = []
+for i in range(len(data) - 2):
+    num1, num2, num3 = data[i:i + 3]
+    u1 = (len(str(abs(num1))) == 3) and str(num1)[-1] == '3'
+    u2 = (len(str(abs(num1))) == 3)
+    г3 = (len(str(abs(num1))) == 3)
+
+    if :
+        if num1 + num2 + num3 >= max_15:
+            ans.append(num1 + num2 + num3)
+print(len(ans), max(ans))
+
+# otvet:
