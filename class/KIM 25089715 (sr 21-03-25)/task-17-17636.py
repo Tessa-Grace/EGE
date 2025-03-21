c09cc4ac6492a20da7a1650b5b35f3d135f28559
Tessa-Grace <tessagerasimova@gmail.com>
2025-03-21 18:36:17 +0200

    solution of 17636

--- class/KIM 25089715 (sr 21-03-25)/task-17-17636.py
+++ class/KIM 25089715 (sr 21-03-25)/task-17-17636.py
@@ -6,11 +6,11 @@ ans = []
 for i in range(len(data) - 2):
     num1, num2, num3 = data[i:i + 3]
     u1 = (len(str(abs(num1))) == 3) and str(num1)[-1] == '3'
-    u2 = (len(str(abs(num1))) == 3) and str(num1)[-1] == '3'
-    u3 = (len(str(abs(num1))) == 3) and str(num1)[-1] == '3'
+    u2 = (len(str(abs(num2))) == 3) and str(num2)[-1] == '3'
+    u3 = (len(str(abs(num3))) == 3) and str(num3)[-1] == '3'
 
     if u1 + u2 + u3 >= 1 and num1 + num2 + num3 <= max_3:
         ans.append(num1 + num2 + num3)
 print(len(ans), max(ans))
 
-# otvet: 52 944
+# otvet: 147 944
