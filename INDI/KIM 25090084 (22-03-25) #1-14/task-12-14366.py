11bfe3f9903b2818cda23e85516449204b037415
Tessa-Grace <tessagerasimova@gmail.com>
2025-03-22 18:00:56 +0200

    solution of 14366

--- INDI/KIM 25090084 (22-03-25) #1-14/task-12-14366.py
+++ INDI/KIM 25090084 (22-03-25) #1-14/task-12-14366.py
@@ -1,13 +1,11 @@
-ans = []
-
 for n in range(4, 10000):
     st = '9' + n * '6'
     while '96' in st or '6666' in st or '1166' in st:
-        st = st.replace('96', '11')
-        st = st.replace('6666', '9')
-        st = st.replace('1166', '69')
-        if sum(map(int, st)) % 37 == 0:
-            ans.append(n)
-print(min(ans))
+        st = st.replace('96', '11', 1)
+        st = st.replace('6666', '9', 1)
+        st = st.replace('1166', '69', 1)
+    if sum(map(int, st)) % 37 == 0:
+        print(n)
+        break
 
-# otvet: 19
+# otvet: 58
