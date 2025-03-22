ab4302c1f347bc2b71f9c224a1cd23969d154096
Tessa-Grace <tessagerasimova@gmail.com>
2025-03-22 15:13:25 +0200

    solution of 18580

---
+++ INDI/KIM 25090084 (22-03-25) #1-14/task-14-15327.py
@@ -0,0 +1,19 @@
+from string import printable
+
+def convert(num, sys):
+    res = ''
+    while num:
+        res += printable[num % sys]
+        num //= sys
+    return res[::-1]
+num = 3*2187**2020 + 3*729**2021 - 2*81**2022 + 27**2023 - 4*3**2024 - 2029
+num = convert(num, 27)
+
+count = 0
+for i in num:
+    if i in printable[10:]:
+        count += 1
+
+print(count)
+
+# otvet: 3368
