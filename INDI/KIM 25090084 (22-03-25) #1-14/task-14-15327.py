1710bc9e8acfa9d6209ed292a7d08f372501434a
Tessa-Grace <tessagerasimova@gmail.com>
2025-03-22 15:14:02 +0200

    solution of 15327

--- INDI/KIM 25090084 (22-03-25) #1-14/task-14-15327.py
+++ INDI/KIM 25090084 (22-03-25) #1-14/task-14-15327.py
@@ -6,6 +6,7 @@ def convert(num, sys):
         res += printable[num % sys]
         num //= sys
     return res[::-1]
+
 num = 3*2187**2020 + 3*729**2021 - 2*81**2022 + 27**2023 - 4*3**2024 - 2029
 num = convert(num, 27)
 
