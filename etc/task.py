9306cff96d9d48fc99e33d127287c6b3b1d8e6be
Tessa-Grace <tessagerasimova@gmail.com>
2025-03-26 22:42:15 +0200

    16 oge var2

--- etc/task.py
+++ etc/task.py
@@ -1,10 +1,15 @@
-num = int(input())
+students = int(input())
+count_students = 0
+maxx = 0
 ans = []
-while num != 0:
-    if num % 8 == 0:
-        ans.append(num)
-    num = int(input())
-if len(ans) > 0:
-    print(sum(ans) / len(ans))
+while count_students != students :
+    q = int(input())
+    count_students += 1
+    ans.append(q)
+    maxx = max(ans)
+
+print(maxx)
+if ans.count(0) > 0:
+    print('YES')
 else:
     print('NO')
