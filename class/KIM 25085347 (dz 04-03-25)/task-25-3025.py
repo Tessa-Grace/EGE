818a0d8c1244498d36c43f470deb85c7c7e3e35d
Tessa-Grace <tessagerasimova@gmail.com>
2025-03-27 18:13:58 +0200

    solution of 3025

--- class/KIM 25085347 (dz 04-03-25)/task-25-3025.py
+++ class/KIM 25085347 (dz 04-03-25)/task-25-3025.py
@@ -1,30 +1,29 @@
 def dividers(num):
     res = set()
     for i in range(2, int(num ** .5) + 1):
-        if num % i == 0:    # проверка: является ли i делителем
-            if i % 2 != 0:  # проверка i на нечетность
+        if num % i == 0:
+            if i % 2 != 0:
                 res |= {i}
-            if num // i % 2 != 0: # проверка результата деления num на i на нечетность
+            if num // i % 2 != 0:
                 res |= {num // i}
 
     res = sorted(res)
-    if len(res) > 5:
+    if len(res) >= 6:
         d = list(res)[-6]
         return d
     return 0
 
 count = 0
-for i in range(200_000_000, 10 ** 20):
-    res = dividers(i)
+for num in range(200_000_001, 10 ** 20):
+    res = dividers(num)
     if res:
-        print(i, res)
+        print(num, res)
         count += 1
         if count == 5:
             break
-
 # otvet:
-# 200000000 125
 # 200000003 48391
 # 200000004 42123
 # 200000005 5
 # 200000008 5101
+# 200000009 113443
--- etc/task.py
+++ etc/task.py
@@ -1,11 +1,29 @@
-num = int(input())
+def dividers(num):
+    res = set()
+    for i in range(2, int(num ** .5) + 1):
+        if num % i == 0:
+            if i % 2 != 0:
+                res |= {i}
+            if num // i % 2 != 0:
+                res |= {num // i}
 
-ans = []
-while num != 0:
-    if num % 8 == 0:
-        ans.append(num)
-    num = int(input())
-if len(ans) > 0:
-    print(sum(ans) / len(ans))
-else:
-    print('NO')
+    res = sorted(res)
+    if len(res) >= 6:
+        d = list(res)[-6]
+        return d
+    return 0
+
+count = 0
+for num in range(200_000_001, 10 ** 20):
+    res = dividers(num)
+    if res:
+        print(num, res)
+        count += 1
+        if count == 5:
+            break
+# otvet:
+# 200000003 48391
+# 200000004 42123
+# 200000005 5
+# 200000008 5101
+# 200000009 113443
