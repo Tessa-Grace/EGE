da2f43895cbc82aea5d60c64c4063ff1b271fa85
Tessa-Grace <tessagerasimova@gmail.com>
2025-04-10 13:26:01 +0200

    solution of 1863

--- class/KIM 25089716 (28-03-25) #19-21 code/task-19-21-1863.py
+++ class/KIM 25089716 (28-03-25) #19-21 code/task-19-21-1863.py
@@ -1,9 +1,9 @@
-def f(x, s): #x- изначально камней , s- кол-во шагов
+def f(x, s): #x- изначально камней, s- кол-во шагов
     if x >= 40: return s % 2 == 0
     if s == 0: return False
     h = [f(x + 1, s - 1), f(x + 4, s - 1), f(x * 2, s - 1)] # h- ходы
-    return any(h) if (s - 1) % 2 == 0 else all(h) # если четный шаг, тогда достаточно одного true, если нечетно, тогда все должно быть true
-
+    return any(h) if (s - 1) % 2 == 0 else all(h) # если четный шаг, тогда достаточно одного true,
+                                                  # если нечетно, тогда все должно быть true
 
 print('19)', [s for s in range(1,40) if f(s, 2)])
 print('20)', [s for s in range(1, 40) if f(s, 3) and not f(s, 1)])
