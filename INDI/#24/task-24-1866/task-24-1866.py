1acbe1606dc99932d659838df8070eb16412a9b5
Tessa-Grace <tessagerasimova@gmail.com>
2025-04-12 17:48:20 +0200

    solution of 1866

--- INDI/#24/task-24-1866/task-24-1866.py
+++ INDI/#24/task-24-1866/task-24-1866.py
@@ -1,10 +1,13 @@
-from re import findall
+from re import *
 
 with open('24_1866.txt') as file:
     data = file.readline()
 
 pattern = r'(?<=ad|da)\w+?(?=ad|da)'
-matches = findall(pattern, data)
+
+matches = finditer(pattern, data)
+
+matches = [i.group() for i in matches]
 print(len(max(matches, key=len)) + 2)
 
 # otvet: 2252
