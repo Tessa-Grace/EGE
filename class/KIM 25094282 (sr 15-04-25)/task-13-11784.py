806885d28ec5780bde4292d2ea360780abddfa21
Tessa-Grace <tessagerasimova@gmail.com>
2025-04-15 18:22:56 +0200

    solution of 11784

---
+++ class/KIM 25094282 (sr 15-04-25)/task-13-11784.py
@@ -0,0 +1,12 @@
+from ipaddress import ip_network
+
+for A in range(255):
+    net = ip_network(f'248.112.{A}.35/255.255.255.240', False)
+    for i in net:
+        i = f'{int(i):032b}'
+        if not(i[:16].count('0') <= i[16:].count('0')):
+            break
+    else:
+        print(A)
+
+# otvet: 224
