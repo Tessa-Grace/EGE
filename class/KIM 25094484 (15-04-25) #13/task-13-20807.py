a8a544a1e50c6d8a2fba6dd27e30dd396522bd50
Tessa-Grace <tessagerasimova@gmail.com>
2025-04-15 19:32:11 +0200

    solution of 20807

---
+++ class/KIM 25094484 (15-04-25) #13/task-13-20807.py
@@ -0,0 +1,11 @@
+from ipaddress import ip_network, ip_address
+
+net = ip_network('172.16.192.0/255.255.192.0')
+cnt = 0
+for i in net:
+    i = f'{int(i):032b}'
+    if i.count('1') % 5 != 0:
+        cnt += 1
+print(cnt)
+
+# otvet: 13003
