f08a791b6b2a9ca2700bbf53b81572c9a635c50e
Tessa-Grace <tessagerasimova@gmail.com>
2025-04-15 16:22:46 +0200

    solution of 17526

---
+++ class/KIM 25093689 (11-04-25) #13/task-13-17526.py
@@ -0,0 +1,12 @@
+from ipaddress import ip_network
+
+net = ip_network('172.16.128.0/255.255.192.0')
+
+count = 0
+for i in net:
+    i = f'{int(i):032b}'
+    if i.count('1') % 2 != 0:
+        count += 1
+print(count)
+
+# otvet: 8192
