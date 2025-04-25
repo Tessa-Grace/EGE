97807de05dc598eea2f11e52311e1f135fc293c4
Tessa-Grace <tessagerasimova@gmail.com>
2025-04-25 10:30:59 +0200

    solution of 5483

---
+++ class/KIM 25094984 (dz 25-04-25) #full/task-2-5483.py
@@ -0,0 +1,15 @@
+from itertools import *
+
+def f(w, x, y, z):
+    return (z == (not x)) <= ((w <= (not y)) and (y <= x))
+
+for a1, a2, a3, a4, a5 in product([0, 1], repeat=5):
+    table = [(1, 1, 1, 0),
+             (a1, a2, 0, 0),
+             (a3, 0, a4, a5)]
+    if len(table) == len(set(table)):
+        for p in permutations('wxyz'):
+            if [f(**dict(zip(p, t))) for t in table] == [1, 0, 0]:
+                print(*p)
+
+# otvet: yzxw
