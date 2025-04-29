51fbf3606647b61ff1917c976f583b0b158b158b
TGerasimova <tessagerasimova@gmail.com>
2025-04-29 18:52:57 +0200

    solution of 14438

--- class/KIM 25097415 (sr 29-04-25)/task-25-14438.py
+++ class/KIM 25097415 (sr 29-04-25)/task-25-14438.py
@@ -3,6 +3,7 @@ from fnmatch import fnmatch
 for i in range(86513, 10 ** 12, 86513):
      if fnmatch(str(i), '17*46??8'):
          if sum(map(int, str(i))) ** .5 % 1 == 0: # sum(map(int, str(i))) ** .5 == int(sum(map(int, str(i))) ** .5)
+# sqrt(sum(map(int, str(i)))) == isqrt(sum(map(int, str(i))))
             print(i, i // 86513)
 # otvet:
 # 170083346818 1965986
