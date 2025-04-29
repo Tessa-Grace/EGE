9d098eb373fdd3b7794d45f05805ff4a6eb1cbc7
TGerasimova <tessagerasimova@gmail.com>
2025-04-29 19:06:21 +0200

    solution of 10781

--- class/KIM 25097415 (sr 29-04-25)/task-13-10781.py
+++ class/KIM 25097415 (sr 29-04-25)/task-13-10781.py
@@ -3,11 +3,11 @@ from ipaddress import *
 ip1 = ip_address('112.117.107.70')
 ip2 = ip_address('112.117.121.80')
 
-for mask in range(33):
+for mask in range(31): # Если берем 31, то сразу исключаем адрес сети и широковещательный
     net1 = ip_network(f'{ip1}/{mask}', False)
     net2 = ip_network(f'{ip2}/{mask}', False)
     if net1.network_address == net2.network_address:
-        if ip1 not in (net1.network_address, net1.broadcast_address):
+        if ip1 not in (net1.network_address, net1.broadcast_address): # if net1 == net2 and ip1 in net1.hosts() and ip2 in net1.hosts():
             if ip2 not in (net2.network_address, net2.broadcast_address):
                 print(net1.num_addresses)
 # otvet:
