cb68d453afb4cee6eab44834720265207e992ad9
TGerasimova <tessagerasimova@gmail.com>
2025-04-29 19:02:53 +0200

    solution of 13885

---
+++ class/KIM 25097415 (sr 29-04-25)/task-13-13885.py
@@ -0,0 +1,14 @@
+from ipaddress import ip_network
+
+for mask in range(16, 25):
+    net = ip_network(f'238.51.1.202/{mask}', False)
+    for i in net:
+        i = f'{int(i):032b}'
+        if not(i[:16].count('1') >= i[16:].count('1')):
+            break
+    else:
+        print(net.netmask)
+        break
+
+# 255.255.252.0,а min третий байт -> 252
+# otvet: 252
