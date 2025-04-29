2718fdff419cafa3b97a0bc1983b75086f3005e3
TGerasimova <tessagerasimova@gmail.com>
2025-04-29 19:03:28 +0200

    solution of 10781

---
+++ class/KIM 25097415 (sr 29-04-25)/task-13-10781.py
@@ -0,0 +1,33 @@
+from ipaddress import *
+
+ip1 = ip_address('112.117.107.70')
+ip2 = ip_address('112.117.121.80')
+
+for mask in range(33):
+    net1 = ip_network(f'{ip1}/{mask}', False)
+    net2 = ip_network(f'{ip2}/{mask}', False)
+    if net1.network_address == net2.network_address:
+        if ip1 not in (net1.network_address, net1.broadcast_address):
+            if ip2 not in (net2.network_address, net2.broadcast_address):
+                print(net1.num_addresses)
+# otvet:
+# 4294967296
+# 2147483648
+# 1073741824
+# 536870912
+# 268435456
+# 134217728
+# 67108864
+# 33554432
+# 16777216
+# 8388608
+# 4194304
+# 2097152
+# 1048576
+# 524288
+# 262144
+# 131072
+# 65536
+# 32768
+# 16384
+# 8192 <- otvet
