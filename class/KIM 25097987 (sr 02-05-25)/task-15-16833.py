630f07529a539ea0f00338473310d8a1a93a9a83
TGerasimova <tessagerasimova@gmail.com>
2025-05-02 18:40:31 +0200

    solution of 16833

--- class/KIM 25097987 (sr 02-05-25)/task-15-16833.py
+++ class/KIM 25097987 (sr 02-05-25)/task-15-16833.py
@@ -7,7 +7,7 @@ def f(x):
     return (a and not q) <= (p or q)
 
 ans = []
-line = [x / 10 for x in range(25 * 10, 119 * 10)]
+line = [x + eps for x in range(25, 119) for eps in [0, 0.1, 0.9]] # вместо [x / 10 for x in range(25 * 10, 119 * 10)], чтоб быстрее
 for a1, a2 in combinations(line, 2):
     if all(f(x) for x in line):
         ans.append(a2 - a1)
