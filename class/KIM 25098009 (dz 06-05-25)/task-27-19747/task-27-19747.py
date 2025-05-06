1536b847039c2c50d5a5fa2c6b46216259ce13ec
Tessa-Grace <tessagerasimova@gmail.com>
2025-05-06 21:08:02 +0200

    solution of 19747

--- class/KIM 25098009 (dz 06-05-25)/task-27-19747/task-27-19747.py
+++ class/KIM 25098009 (dz 06-05-25)/task-27-19747/task-27-19747.py
@@ -31,7 +31,8 @@ py = (centerA1[1] + centerA2[1] + centerA3[1]) / 3
 
 
 print(abs(int(px * 100_000)), abs(int(py * 100_000)))
-#______________________________
+
+#__________________B part
 with open('27B_19747.txt') as file:
     clusterB1 = []
     clusterB2 = []
@@ -42,20 +43,21 @@ with open('27B_19747.txt') as file:
         x, y = map(float, i.split())
         if x < 10 and y < x:
             clusterB1.append([x, y])
-        if x < 10 and y > x:
+        elif x < 10 and y > x:
             clusterB2.append([x, y])
-        if x > 10 and y < 10:
+        elif x > 10 and y < 10:
             clusterB3.append([x, y])
-        if x > 10 and y < 10:
+        elif x > 10 and y > 10 and y < x:
             clusterB4.append([x, y])
         else:
             clusterB5.append([x, y])
 
+
 centerB1 = centroid(clusterB1)
 centerB2 = centroid(clusterB2)
 centerB3 = centroid(clusterB3)
-centerB4 = centroid(clusterB2)
-centerB5 = centroid(clusterB3)
+centerB4 = centroid(clusterB4)
+centerB5 = centroid(clusterB5)
 
 px = (centerB1[0] + centerB2[0] + centerB3[0] + centerB4[0] + centerB5[0]) / 5
 py = (centerB1[1] + centerB2[1] + centerB3[1] + centerB4[1] + centerB5[1]) / 5
@@ -64,53 +66,5 @@ py = (centerB1[1] + centerB2[1] + centerB3[1] + centerB4[1] + centerB5[1]) / 5
 print(abs(int(px * 100_000)), abs(int(py * 100_000)))
 
 # A) 532496 533164
-# B) ne znayu :(
-
-
-#
-# from math import dist
-#
-# def centroid(cluster):
-#     dists = []
-#     for dot in cluster:
-#         sum_dist = 0
-#         for dot2 in cluster:
-#             sum_dist += dist(dot, dot2)
-#         dists.append([sum_dist, dot])
-#     return min(dists)[1]
-#
-# with open('27B_19747.txt') as file:
-#     clusterB1 = []
-#     clusterB2 = []
-#     clusterB3 = []
-#     clusterB4 = []
-#     clusterB5 = []
-#     for i in file:
-#         x, y = map(float, i.split())
-#         if x < 10 and y < x:
-#             clusterB1.append([x, y])
-#         elif x < 10 and y > x:
-#             clusterB2.append([x, y])
-#         elif x > 10 and y < 10:
-#             clusterB3.append([x, y])
-#         elif x > 10 and y > 10 and y < x:
-#             clusterB4.append([x, y])
-#         else:
-#             clusterB5.append([x, y])
-#
-#
-# centerB1 = centroid(clusterB1)
-# centerB2 = centroid(clusterB2)
-# centerB3 = centroid(clusterB3)
-# centerB4 = centroid(clusterB4)
-# centerB5 = centroid(clusterB5)
-#
-# px = (centerB1[0] + centerB2[0] + centerB3[0] + centerB4[0] + centerB5[0]) / 5
-# py = (centerB1[1] + centerB2[1] + centerB3[1] + centerB4[1] + centerB5[1]) / 5
-#
-#
-# print(abs(int(px * 100_000)), abs(int(py * 100_000)))
-#
-# # A) 532496 533164
-# # B) ne znayu :(
+# B) 1091104 954833
 
