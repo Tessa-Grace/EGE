c983fa6951b4a04b3cc8053a46e6b93bdf113e67
TGerasimova <tessagerasimova@gmail.com>
2025-05-06 19:38:13 +0200

    solution of 20497

---
+++ class/KIM 25098724 (06-05-25) #27/task-27-20497/task-27-20497.py
@@ -0,0 +1,38 @@
+from math import dist
+
+def centroid(cluster):
+    dists = []
+    for dot in cluster:
+        sum_dists = 0
+        for dot2 in cluster:
+            sum_dists += dist(dot, dot2)
+        dists.append([sum_dists, dot])
+    return max(dists)[1]
+
+with open('27.19.B_20497.txt') as file:
+    data = [list(map(float, i.replace(',', '.').split())) for i in file]
+
+clusters = []
+eps = 4.5 <- # ищем мин расстояние от каждой возможной аномалии до точки из кластера
+while data:
+    cluster = [data.pop()]
+    for dot in cluster:
+        for dot2 in data.copy():
+            if dist(dot, dot2) < eps:
+                cluster.append(dot2)
+                data.remove(dot2)
+    if len(cluster) > 10:
+        clusters.append(cluster)
+print([len(cluster) for cluster in clusters])
+
+centers = [centroid(cluster) for cluster in clusters]
+px = sum(center[0] for center in centers) / len(centers)
+py = sum(center[1] for center in centers) / len(centers)
+
+print(int(px * 10_000), int(py * 10_000))
+
+# otvet:
+# [286, 296, 302] <- кластеры
+# A) 13258 2656
+# [1985, 2132, 1915, 1948, 2004] <- кластеры
+# B) -209434 474989
