7dffd252221218a96e53f04ea17b716cfeb49d74
Tessa-Grace <tessagerasimova@gmail.com>
2025-05-13 19:36:23 +0200

    solution of 4682

---
+++ class/KIM 25099953 (13-05-25) # 24 (1)/task-24-4682 #2 type/task-24-4682.py
@@ -0,0 +1,17 @@
+with open('24_4682.txt') as file:
+    data = file.readline()
+
+for i in 'CD':
+    data = data.replace(i, 'B')
+
+data = data.replace('E', 'A')
+data = data.replace('AB', '*')
+
+for i in 'ABCD':
+    data = data.replace(i, ' ')
+
+data = data.split()
+
+print(len(max(data, key=len)))
+
+# otvet: 202
