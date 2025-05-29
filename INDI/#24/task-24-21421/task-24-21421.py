d0bdd83dc7e434762384f7676128b8b0d767379e
Tessa-Grace <tessagerasimova@gmail.com>
2025-05-29 18:21:52 +0200

    solution of 21421

--- INDI/#24/task-24-21421/task-24-21421.py
+++ INDI/#24/task-24-21421/task-24-21421.py
@@ -7,3 +7,21 @@ m = [i.group() for i in finditer(pattern, st)]
 print(len(max(m, key=len)))
 
 # otvet: 19
+
+from string import ascii_uppercase
+
+with open('') as file:
+    data = file.readline()
+
+for i in ascii_uppercase[2:]:
+    data = data.replace(i, ' ')
+
+data = data.split()
+ans = 0
+for i in data:
+    i = i.rstrip('13579B').lstrip('0')
+    ans = max(len(i), ans)
+
+print(ans)
+
+# otvet: 19
