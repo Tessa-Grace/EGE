e35d0594b09d3e3de0fc4bb567e9a6d9936dd3cc
Tessa-Grace <tessagerasimova@gmail.com>
2025-05-29 17:45:29 +0200

    solution of 4602

--- INDI/#24/task-24-4602/task-24-4602.py
+++ INDI/#24/task-24-4602/task-24-4602.py
@@ -11,3 +11,12 @@ matches = [i.group() for i in matches]
 print(len(max(matches, key=len)) // 2)
 
 # otvet: 174
+
+from re import *
+with open('24_4602.txt') as f:
+    st = f.readline()
+pattern = r'([BCD][AO])+'
+match = finditer(pattern, st)
+m = [i.group() for i in match]
+print(len(max(m, key=len))//2)
+# otvet: 174
