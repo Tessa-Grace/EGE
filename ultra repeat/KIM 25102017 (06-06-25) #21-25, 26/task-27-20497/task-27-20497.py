d75f700bebc1f0100178b1135a1501f11deb87bd
Tessa-Grace <tessagerasimova@gmail.com>
2025-06-07 13:04:07 +0200

    solution

--- ultra repeat/KIM 25102017 (06-06-25) #21-25, 26/task-27-20497/task-27-20497.py
+++ ultra repeat/KIM 25102017 (06-06-25) #21-25, 26/task-27-20497/task-27-20497.py
@@ -9,11 +9,11 @@ def centroid(cluster):
         dists.append([sum_dists, dot])
     return max(dists)[1]
 
-with open('27.19.A_20497.txt') as file:
+with open('27.19.B_20497.txt') as file:
     data = [list(map(float, i.replace(',', '.').split())) for i in file]
 
 clusters = []
-eps = 0.5 # <- ищем мин расстояние от каждой возможной аномалии до точки из кластера
+eps = 4.5 # <- ищем мин расстояние от каждой возможной аномалии до точки из кластера
 while data:
     cluster = [data.pop()]
     for dot in cluster:
@@ -29,10 +29,10 @@ centers = [centroid(cluster) for cluster in clusters]
 px = sum(center[0] for center in centers) / len(centers)
 py = sum(center[1] for center in centers) / len(centers)
 
-print(int(px * 10_000), int(py * 10_000))
+print(abs(int(px * 10_000)), abs(int(py * 10_000)))
 
 # otvet:
 # [286, 296, 302] <- кластеры eps 0.5
 # A) 13258 2656
 # [1985, 2132, 1915, 1948, 2004] <- кластеры eps = 4.5
-# B) -209434 474989
+# B) 209434 474989
